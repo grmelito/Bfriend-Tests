cea26ffa1e20bb3dfdbcaee5e84fbe4dcd7fec1c
grmelito <gr.melito@hotmail.com>
2020-07-25 20:24:32 -0300

    Adicionando resultados de testes

--- e2e/config/protractor.conf.js
+++ e2e/config/protractor.conf.js
@@ -1,7 +1,24 @@
 const { browser } = require('protractor');
+const SpecReporter = require('jasmine-allure-reporter');
 
 module.exports.config = {
-    spec: ['../spec/*.spec.js'],
+    specs: ['../spec/*.spec.js'],
+    onPrepare: () => {
+        jasmine.getEnv().addReporter(new SpecReporter({
+            suite: {
+                displayNumber: true
+            },
+            spec: {
+                displayFailed: true,
+                displayPending: true,
+                displayDuration: true,
+                displayStackTrace: true
+            },
+            summary: {
+                displayFailed: true
+            }
+        }))
+    },
     capabilities: {
         'browserName': 'firefox'
     },

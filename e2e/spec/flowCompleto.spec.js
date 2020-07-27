f7a11d70dc21aadbb4791f13e82ce31f6c150e95
grmelito <gr.melito@hotmail.com>
2020-07-27 13:21:13 -0300

    Testes de upgrade e criação de anuncio

--- e2e/spec/flowCompleto.spec.js
+++ e2e/spec/flowCompleto.spec.js
@@ -4,6 +4,8 @@ const Cadastro = require('../page_objects/cadastro.po')
 const Login = require('../page_objects/login.po')
 const Home = require('../page_objects/home.po')
 const HomeLogada = require('../page_objects/homeLogada.po')
+const CadastroFornecedor = require('../page_objects/cadastroFornecedor.po')
+const CadastroLoja = require('../page_objects/cadastroLoja.po')
 
 
 describe('Flow Completo', () => {
@@ -34,9 +36,33 @@ describe('Flow Completo', () => {
     it('Deve buscar anuncios', () => {
         browser.waitForAngularEnabled(false)
         browser.get('http://localhost:3000/homeLogada');
-        browser.sleep(2000)
+        browser.sleep(1000)
         HomeLogada.buscarAnuncio('Centro')
 
         expect(HomeLogada.sectionCards.isPresent()).toBe(true)
     })
+
+    it('Deve virar fornecedor', () => {
+        browser.waitForAngularEnabled(false)
+        browser.get('http://localhost:3000/cadastroFornecedor');
+        CadastroFornecedor.virarFornecedor('44532115689', '11065489', 'Rua Almirante', '32', 'Apt 14', 'Centro')
+
+        const alertDialog = browser.switchTo().alert();
+        browser.sleep(2000)
+        expect(alertDialog.getText()).toEqual('Upgrade Realizado com sucesso!')
+        alertDialog.dismiss()
+    })
+
+    it('Deve cadastrar um anuncio', () => {
+        browser.waitForAngularEnabled(false)
+        browser.get('http://localhost:3000/cadastroLoja');
+        CadastroLoja.cadastrarAnuncio('Security Tech', 'SecTech', 'SecTechOficial',
+         'https://SecTech.com', 'Nós somos uma empresa de consultoria com foco em segurança', '13996009843')
+
+        const alertDialog = browser.switchTo().alert();
+        browser.sleep(1000)
+        expect(alertDialog.getText()).toEqual('Anuncio Cadastrado com succeso!')
+        alertDialog.dismiss()
+    })
+
 })

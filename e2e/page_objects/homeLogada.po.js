34738164c908357cd553b3877f80795ed61cd010
grmelito <gr.melito@hotmail.com>
2020-07-27 01:05:24 -0300

    Adicionando teste de busca

---
+++ e2e/page_objects/homeLogada.po.js
@@ -0,0 +1,28 @@
+const { browser, element, ExpectedConditions } = require('protractor')
+const protractorHelper = require("protractor-helper");
+
+class HomeLogada {
+    constructor() {
+        this.cidade = element(by.id('cidade'))
+        this.bairro = element(by.id('Bairro'))
+        this.categoria = element(by.id('categoria'))
+        this.buscarButton = element(by.buttonText('Buscar'))
+        this.fornecedorButton = element(by.buttonText('Vire Fornecedor'))
+        this.sectionCards = element(by.id('card-Reparos')).element(by.css('.container-cards'))
+    };
+
+    buscarAnuncio(bairro) {
+        protractorHelper.hoverAndClick(this.cidade)
+        browser.sleep(1000)
+        this.cidade.sendKeys('Santos')
+        //const cidadeEscolhida = this.cidade.element(by.css('option[value="Santos"]'))
+        //protractorHelper.hoverAndClick(cidadeEscolhida);
+        this.bairro.sendKeys(bairro);
+        protractorHelper.hoverAndClick(this.categoria)
+        browser.sleep(1000)
+        this.categoria.sendKeys('Artesanato')
+        this.buscarButton.click();
+    };
+}
+
+module.exports = new HomeLogada();
--- e2e/spec/flowCompleto.spec.js
+++ e2e/spec/flowCompleto.spec.js
@@ -3,10 +3,12 @@ const { browser, element, protractor } = require('protractor')
 const Cadastro = require('../page_objects/cadastro.po')
 const Login = require('../page_objects/login.po')
 const Home = require('../page_objects/home.po')
+const HomeLogada = require('../page_objects/homeLogada.po')
+
 
 describe('Flow Completo', () => {
 
-    it('Realizar cadastro de conta', () => {
+    it('Deve Realizar cadastro de conta', () => {
         browser.waitForAngularEnabled(false)
         browser.get('http://localhost:3000/')
         Home.clicarCadastro()
@@ -20,12 +22,21 @@ describe('Flow Completo', () => {
 
     })
 
-    it('Fazer login', () => {
+    it('Deve Fazer login', () => {
         browser.waitForAngularEnabled(false)
         browser.get('http://localhost:3000/login');
 
         Login.realizarLogin('claire@gmail.com', 'claire123')
 
         expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/homeLogada');
+    })
+
+    it('Deve buscar anuncios', () => {
+        browser.waitForAngularEnabled(false)
+        browser.get('http://localhost:3000/homeLogada');
+        browser.sleep(2000)
+        HomeLogada.buscarAnuncio('Centro')
+
+        expect(HomeLogada.sectionCards.isPresent()).toBe(true)
     })
 })
